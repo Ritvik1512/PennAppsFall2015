585d5887a0e82dbc56c1cada3a2283bde2ebf785
Justin He <justinheeftw@gmail.com>
2015-09-05 16:31:49 -0400

    registration and user system set up.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # PennAppsFall2015
-A repository for the Justin's, Ritvik's, and Frederick's project built at PennApps Fall 2015.
+A repository for the Justin's project built at PennApps Fall 2015.
---
+++ server.js
@@ -0,0 +1,99 @@
+
+var PORT_NUMBER = process.env.PORT || 8888;
+
+// DEPENDENCIES, NODE MODULE LOAD IN
+// FOR PARSING AND AUTHORIZATION
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var morgan = require('morgan');
+var methodOverride = require('method-override');
+var flash = require('connect-flash');
+
+// SWIG TEMPLATING ENGINE
+var swig = require('swig');
+
+// FOR OBJECT ORIENTATED MODELS 
+var mongoose = require('mongoose');
+var User = require('./models/user');
+
+// FOR AUTHORIZATION OF LOCAL ACCOUNTS
+var passport = require('passport');
+var passportlocal = require('passport-local');
+var LocalStrategy   = require('passport-local').Strategy;
+
+// SOUNDCLOUD NODE MODULE
+var SC = require('soundclouder');
+// LINKING SECRET FILES FOR SOUNDCLOUD AUTH
+var SCAuthInfo = require('./config/soundcloud');
+
+SC.init(SCAuthInfo.CLIENT_ID, SCAuthInfo.CLIENT_SECRET, '/');
+
+// ROUTING ENGINE
+var express = require('express');
+var app = express();
+
+
+// LINKING MongoDB DATABASE INFO
+var configDB = require('./config/database')
+
+//CONFIGURATION
+mongoose.connect("localhost:27017/pennapps"); // connect to our database
+app.engine('html', swig.renderFile);
+
+app.set('port', PORT_NUMBER);
+app.set('view engine', 'html');
+app.use(morgan('combined'));
+app.use('/bower_components',
+        express.static(__dirname + '/bower_components'));
+app.use('/static',
+        express.static(__dirname + '/static'));
+
+require('./config/passport')(passport); // pass passport for configuration
+
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+app.use(methodOverride());
+app.use(session({secret: 'hello'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+
+//ROUTING
+app.get('/', function(request, response) {
+  response.render('index.html');
+});
+
+app.get('/profile', function(request, response) {
+  response.render('profile.html');
+});
+
+app.get('/login', function(request, response) {
+  response.render('login.html');
+});
+
+app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+}));
+
+app.get('/register', function(request, response) {
+  response.render('register.html');
+})
+
+app.post('/register', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/register', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+}));
+
+//SERVER LAUNCH
+var server = require('http').Server(app);
+server.listen(PORT_NUMBER, function() {
+  console.log('Listening to port ' + PORT_NUMBER);
+});
